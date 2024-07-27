7aede1e4284687c0fe5471c0f5180053417f7919
1cbyc <isaacnsisong@gmail.com>
2024-07-27 23:09:14 +0100

    added the main python script

---
+++ main.py
@@ -0,0 +1,15 @@
+from scraper.scraper import scrape_multiple_pages
+from scraper.database import close_connection
+
+
+def main():
+    base_url = 'http://nsisong.com/'  # will modify later to take data from a list of url
+    num_pages = 5  # can adjust the number of pages to scrape
+    scrape_multiple_pages(base_url, num_pages)
+
+    # then close the database connection after scraping
+    close_connection()
+
+
+if __name__ == "__main__":
+    main()
