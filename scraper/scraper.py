0161547a1525850e781fc02fcf47f9022662cc6a
1cbyc <isaacnsisong@gmail.com>
2024-07-27 23:14:01 +0100

    updated the scraper.py file with headers to simulate browser

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -4,7 +4,14 @@ from .database import save_to_db
 
 
 def scrape_page(url):
-    response = requests.get(url)
+    # i want to mimic a browser cause of the 403 error, so i will add a headers
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+    # response = requests.get(url)
+    # instead of just running request directly with the url like i did above, i will add the headers like this:
+    response = requests.get(url, headers=headers)
+
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -13,7 +20,7 @@ def scrape_page(url):
             if href:
                 save_to_db(href)
     else:
-        print(f"Failed to retrieve the page. Status code: {response.status_code}")
+        print(f"hey chief i could not retrieve the page o. Status code: {response.status_code}")
 
 
 def scrape_multiple_pages(base_url, num_pages):
