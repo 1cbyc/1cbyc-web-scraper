5422de4ac7b34984db48385ebc57f697cdfc8112
1cbyc <isaacnsisong@gmail.com>
2024-08-03 05:56:42 +0100

    i enjoy this project

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template, redirect, url_for
+from scraper.scraper import scrape_multiple_pages
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        base_url = request.form['base_url']
+        num_pages = int(request.form['num_pages'])
+
+        if not base_url or num_pages <= 0:
+            return "Invalid input, please try again."
+
+        # Start scraping
+        scrape_multiple_pages(base_url, num_pages)
+        return redirect(url_for('index'))
+
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
