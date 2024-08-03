a5e33aaa04d124d31d8417eb508cb905fcae13ee
1cbyc <isaacnsisong@gmail.com>
2024-08-03 07:36:37 +0100

    refactoring as a web app

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def index():
     return render_template('index.html')
 
 # @app.route('/download/<db_name>')
-@app.route('/<db_name>')
+@app.route('/download/<db_name>')
 def download(db_name):
     file_path = os.path.join('data', db_name)
     if os.path.exists(file_path):
